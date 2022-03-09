a12513e9a961bed84e49976a11b6a27bee04d2d3
Yong Jin <jinyong@YongdeMBP.lan>
2022-03-10 05:52:28 +0800

    initial commit

---
+++ FirstC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  FirstC
+//
+//  Created by Yong Jin on 2022/3/10.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
